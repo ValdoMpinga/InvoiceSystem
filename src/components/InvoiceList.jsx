ec53f7a3e6a760d27f610e0a9d61cc1cb74e2a66
Valdo Mpinga <valdompinga2@gmail.com>
2023-11-29 20:05:40 +0000

    invoice list rendering done

---
+++ src/components/InvoiceList.jsx
@@ -0,0 +1,68 @@
+import React, {useEffect} from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+const windowWidth = Dimensions.get('window').width;
+
+const InvoiceList = ({invoicesData}) => {
+  useEffect(() => {
+    console.log('here');
+    console.log(invoicesData);
+  }, [invoicesData]); // Add invoicesData as a dependency
+
+  const renderInvoiceItem = ({item}) => {
+    // Convert the emissionDate string to a JavaScript Date object
+    const dateObject = new Date(item.emissionDate);
+
+    // Format the date using Intl.DateTimeFormat
+    const formattedDate = new Intl.DateTimeFormat('en-GB', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    }).format(dateObject);
+
+    return (
+      <View style={styles.card}>
+        <TouchableOpacity>
+          <Text style={styles.title}>Invoice Status: {item.status}</Text>
+          <Text>Emission Date: {formattedDate}</Text>
+          <Text>Total Amount: {item.totalAmount}</Text>
+          <Text>Customer Email: {item.customerEmail}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  return (
+    <FlatList
+      data={invoicesData}
+      keyExtractor={invoice => invoice.status.toString()}
+      renderItem={renderInvoiceItem}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    width: windowWidth/1.2,
+    backgroundColor: 'red',
+    padding: 15,
+    margin: 10,
+    borderRadius: 8,
+    elevation: 3,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default InvoiceList;
--- src/screens/InvoiceScreen.jsx
+++ src/screens/InvoiceScreen.jsx
@@ -1,18 +1,73 @@
 import {View, Text} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {setTestString, setShowCustomerModal} from '../../redux/invoiceStore';
 import {Colors} from '../utils/contants';
 import FloatingButton from '../components/FloatingButtons';
+import InvoiceList from '../components/InvoiceList';
+import {API_URL} from '../utils/contants';
 
 const InvoiceScreen = ({navigation}) => {
   const dispatch = useDispatch();
+  const [invoices, setInvoices] = useState([]);
 
   const {testString} = useSelector(state => state.invoice);
 
   useEffect(() => {
-    navigation.navigate('WithoutTabs', {screen: 'InvoiceCreation'});
+    fetchInvoices();
   }, []);
+
+const fetchInvoices = async () => {
+  try
+  {
+    const response = await fetch(`${API_URL}/invoice/get-invoice-list`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      console.error('Failed to fetch invoices');
+      return;
+    }
+
+    const data = await response.json();
+
+    // Use Promise.all to wait for all asynchronous operations to complete
+    const invoicesArray = await Promise.all(
+      data.invoices.map(async invoice => {
+        const customerResponse = await fetch(`${API_URL}/customer/get-by-id`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({customer_id: invoice.customer_id}),
+        });
+
+        const customerObj = await customerResponse.json();
+
+        return {
+          status: 'Paid',
+          emissionDate: invoice.emission_data,
+          totalAmount: invoice.total_amount,
+          customerEmail: customerObj.customer.email,
+        };
+      }),
+    );
+
+    // Log the content of invoicesArray
+    console.log('Invoices Array:', invoicesArray);
+
+    // Set the state with the new invoicesArray
+    setInvoices(invoicesArray);
+  } catch (error) {
+    console.error('Error fetching invoices:', error);
+    // Handle error, maybe set an error state
+  }
+};
+
+
 
   return (
     <View
@@ -22,11 +77,10 @@ const InvoiceScreen = ({navigation}) => {
         alignItems: 'center',
         backgroundColor: Colors.color5,
       }}>
-      <Text>InvoiceScreen {testString} </Text>
+      <InvoiceList invoicesData={invoices} />
       <FloatingButton
         iconName={'file-invoice'}
         onButtonClick={() => {
-          console.log('ya');
           navigation.navigate('WithoutTabs', {screen: 'InvoiceCreation'});
         }}
       />
